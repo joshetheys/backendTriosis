532fbb007f61860dbe07e26a5abbe79f3b9dac6f
joshetheys <JosheTheys@gmail.com>
2022-09-06 11:23:09 +0200

    added more page

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(express.static('views'))
 // Set header
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader('Access-Control-Allow-Methods',"*");
+    res.setHeader("Access-Control-Allow-Headers-*", "*");
     next();
 });
 app.use(cors({
